d5b8fa98bde7037a40596bd4d5a2a812d76af7b0
Guilherme C Gonzalez <gcgonzalez99@hotmail.com>
2021-03-03 17:42:27 -0300

    refactor: recreate style aside bar

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,13 +1,39 @@
 import styles from '../styles/components/MenuBar.module.css';
+import { FiAward, FiHome, FiLogOut } from 'react-icons/fi';
+import { useEffect, useState } from 'react';
 
 export function MenuBar() {
 
+    const [buttonHome, setButtonHomeState] = useState(true);
+    const [buttonAward, setButtonAwardState] = useState(false);
+
+    function changeHomeButton(){
+        setButtonHomeState(true);
+        setButtonAwardState(false);
+    }
+    function changeAwardButton(){
+        setButtonAwardState(true);
+        setButtonHomeState(false);
+    }
+
+    useEffect(() => {
+        
+    }, [buttonHome, buttonAward])
+
     return (
         <div>
-            <div className={styles.menuBar}>
+            <div className={`${styles.container}`}>
                 <img src="icons/logo.svg" alt="moveit logo"/>
-                <img src="icons/home.svg" alt="home logo"/>
-                <img src="icons/badge.svg" alt="badge logo"/>
+                <div className={styles.buttons}>
+                    <button className={`${styles.selected}`}  onClick={changeHomeButton} type="button" disabled>
+                        <div></div>
+                        <FiHome size={32} />
+                    </button>   
+                    <button className={`${styles.selected}`} onClick={changeAwardButton} type="button" disabled>
+                        <div></div>
+                        <FiAward size={32}/>
+                    </button>
+                </div>
             </div>
         </div>
     );
